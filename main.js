7ed165a62a1cd39582c97764484b4303b8c02d53
CertIanS <irsnider13@gmail.com>
2026-05-07 10:35:16 -0400

    Added JS and styling

---
+++ main.js
@@ -0,0 +1,33 @@
+var rating = 0;
+var buttonIds = ["one", "two", "three", "four", "five"];
+
+function changeRating(id){
+    buttonIds.forEach(bId => {
+        document.getElementById(bId).disabled = false;
+    });
+
+    document.getElementById(id).disabled = true;
+    switch(id){
+        case "one":
+            rating = 1;
+            break;
+        case "two":
+            rating = 2;
+            break;
+        case "three":
+            rating = 3;
+            break;
+        case "four":
+            rating = 4;
+            break;
+        case "five":
+            rating = 5;
+    }
+}
+
+function showThankYouMessage(){
+    document.getElementById("rateState").style.display = "none";
+    document.getElementById("thankYouState").style.display = "block";
+    document.getElementById("userRating").textContent = "" + rating;
+    event.preventDefault();
+}
